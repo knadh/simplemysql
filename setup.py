1f176997b564a5800cd264119349133a643670fe
Kailash Nadh <kailash.nadh@gmail.com>
2013-06-03 20:25:59 +0530

    Switched to setuptools

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="simplemysql",
-    version=0.1,
+    version="0.1",
     description="An ultra simple wrapper for Python MySQLdb with very basic functionality",
     author="Kailash Nadh",
+    author_email="kailash.nadh@gmail.com",
     url="http://nadh.in/code/simplemysql",
     packages=['simplemysql'],
     download_url="http://github.com/knadh/simplemysql",
     license="GPLv2",
     classifiers=[
-        "Development Status :: 0.1 - Alpha",
-        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Natural Language :: English",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-        "Topic :: Internet"
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries"
     ],
     install_requires=["mysql-python"]
 )
