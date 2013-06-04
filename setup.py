164bc104436dd9d3564495e4e6094cba686fca87
Kailash Nadh <kailash.nadh@gmail.com>
2013-06-04 12:36:50 +0530

    git diff setup.py

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@
 from setuptools import setup
 
 setup(
-    name="simplemysql",
-    version="0.1",
-    description="An ultra simple wrapper for Python MySQLdb with very basic functionality",
-    author="Kailash Nadh",
-    author_email="kailash.nadh@gmail.com",
-    url="http://nadh.in/code/simplemysql",
-    packages=['simplemysql'],
-    download_url="http://github.com/knadh/simplemysql",
-    license="GPLv2",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Natural Language :: English",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Database",
-        "Topic :: Software Development :: Libraries"
-    ],
-    install_requires=["mysql-python"]
+	name="simplemysql",
+	version="0.1",
+	description="An ultra simple wrapper for Python MySQLdb with very basic functionality",
+	author="Kailash Nadh",
+	author_email="kailash.nadh@gmail.com",
+	url="http://nadh.in/code/simplemysql",
+	packages=['simplemysql'],
+	download_url="http://github.com/knadh/simplemysql",
+	license="GPLv2",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Natural Language :: English",
+		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Database",
+		"Topic :: Software Development :: Libraries"
+	],
+	install_requires=["mysql-python"]
 )
