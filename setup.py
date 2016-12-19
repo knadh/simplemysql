44ba57a6d287d83b8369a6beede6376397a54252
Kailash Nadh <kailash.nadh@gmail.com>
2016-12-19 09:23:58 +0530

    Version bump for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name="simplemysql",
-	version="1.02",
+	version="1.25",
 	description="An ultra simple wrapper for Python MySQLdb with very basic functionality",
 	author="Kailash Nadh",
 	author_email="kailash.nadh@gmail.com",
